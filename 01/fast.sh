57722ac2c80f0b13f116f5b705fba8e826de109e
ThinkingWavy <camillo.zanolin@gmail.com>
2024-03-11 15:54:30 +0100

    added script and changes make

---
+++ 01/fast.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Execute job in the partition "lva" unless you have special requirements.
+#SBATCH --partition=lva
+# Name your job to be able to identify it later
+#SBATCH --job-name=fast
+# Redirect output stream to this file
+#SBATCH --output=results/%x-O3.out
+# Maximum number of tasks (=processes) to start in total
+#SBATCH --ntasks=1
+# Maximum number of tasks (=processes) to start per node
+#SBATCH --ntasks-per-node=1
+# Enforce exclusive node allocation, do not share with other jobs
+#SBATCH --exclusive
+
+# Check if there's an argument provided
+
+executable="fast"
+
+# Define the number of threads
+threads=(1 4 8 16)
+
+# Loop through the number of threads and execute the program
+for t in "${threads[@]}"; do
+    export OMP_NUM_THREADS=$t
+    echo "Executing $executable with $t threads"
+    /bin/time ./$executable
+    echo ""
+done
