a3b46e78b9c59b4fb9c806996feff02202722d5a
ThinkingWavy <camillo.zanolin@gmail.com>
2024-04-08 15:52:12 +0200

    exercise 1 tasks

--- 03/mandelbrot/mandelbrot.c
+++ 03/mandelbrot/mandelbrot.c
@@ -13,9 +13,29 @@
 #define MAX_ITER 10000
 
 void calc_mandelbrot(uint8_t image[Y][X]) {
-	// TODO: Write your code here!
-}
+	for(int y_pixel = 0; y_pixel < Y; y_pixel++) {
+		for(int x_pixel = 0; x_pixel < X; x_pixel++) {
+			double cx, cy, x, y, x_tmp;
+			int iteration, norm_iteration;
 
+			x = 0.0;
+			y = 0.0;
+			cx = (double)x_pixel / X * 3.5 - 2.5;
+			cy = (double)y_pixel / Y * 2.0 - 1.0;
+			iteration = 0;
+
+			while(x * x + y * y <= 2 * 2 && iteration < MAX_ITER) {
+				x_tmp = x * x - y * y + cx;
+				y = 2 * x * y + cy;
+				x = x_tmp;
+				iteration++;
+			}
+
+			norm_iteration = (int)((double)iteration / MAX_ITER * 255);
+			image[y_pixel][x_pixel] = norm_iteration;
+		}
+	}
+}
 int main() {
 	uint8_t image[Y][X];
 
